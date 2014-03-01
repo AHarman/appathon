f060347bee47125d058fdf9f53e167e1f6a882fc
Andy Provan <ap12750@my.bristol.ac.uk>
2014-03-01 13:24:59 +0000

    Made app fully fullscreen

--- SoundInvaders/src/main/java/com/SI/soundinvaders/MainActivity.java
+++ SoundInvaders/src/main/java/com/SI/soundinvaders/MainActivity.java
@@ -5,13 +5,16 @@ import java.lang.reflect.Field;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.res.Resources;
 import android.opengl.GLSurfaceView;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.ScaleGestureDetector.OnScaleGestureListener;
+import android.view.View;
 
 import com.threed.jpct.Camera;
 import com.threed.jpct.FrameBuffer;
@@ -73,6 +76,8 @@ public class MainActivity extends Activity {
         renderer = new MyRenderer();
         mGLView.setRenderer(renderer);
         setContentView(mGLView);
+
+        hideSystemBars();
     }
 
     @Override
@@ -147,6 +152,22 @@ public class MainActivity extends Activity {
 
     protected boolean isFullscreenOpaque() {
         return true;
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private void hideSystemBars()
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
+        {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            );
+        }
     }
 
     class MyRenderer implements GLSurfaceView.Renderer {
