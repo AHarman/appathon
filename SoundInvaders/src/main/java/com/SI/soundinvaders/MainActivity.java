3124e198d0e3bb5f1badd895ee6b6fd4d7c2637c
AHarman <alexanderjharman@gmail.com>
2014-03-01 21:54:24 +0000

    Better UI hiding for jellybean

--- SoundInvaders/src/main/java/com/SI/soundinvaders/MainActivity.java
+++ SoundInvaders/src/main/java/com/SI/soundinvaders/MainActivity.java
@@ -175,7 +175,7 @@ public class MainActivity extends FragmentActivity implements GestureDetector.On
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     private void hideSystemBars()
     {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
         {
             getWindow().getDecorView().setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
@@ -185,6 +185,8 @@ public class MainActivity extends FragmentActivity implements GestureDetector.On
                             | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
             );
+        } else if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN){
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
         }
     }
 
