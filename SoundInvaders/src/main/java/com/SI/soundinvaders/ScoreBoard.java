679c27b66f32957b7cdc4a5e5cd968d110f61d54
AHarman <alexanderjharman@gmail.com>
2014-03-01 13:26:31 +0000

    Messier than a three year old with a bucket of paint but it kind of sort of works.

--- SoundInvaders/src/main/java/com/SI/soundinvaders/ScoreBoard.java
+++ SoundInvaders/src/main/java/com/SI/soundinvaders/ScoreBoard.java
@@ -14,40 +14,67 @@ import java.util.ArrayList;
 public class ScoreBoard {
 
 
-    private final File dir;
+    private File dir = null;
     private ArrayList<Integer> scores = new ArrayList();
     private ArrayList<String> players = new ArrayList<String>();
     private Context appContext;
 
     ScoreBoard(Context contextual)
     {
-        dir = appContext.getFilesDir();
         appContext = contextual;
-        readScores();
-        Log.d("files", toString());
+        try{
+            dir = appContext.getFilesDir();
+        } catch (Exception e){
+            Log.e("files", "dir " + e.toString());
+        }
+        File scoreFile = appContext.getFileStreamPath("scores.txt");
+        if(scoreFile.exists())
+        {
+            Log.d("files", "FILE EXISTS!");
+            readScores();
+        }
+        else
+        {
+            try{
+                appContext.openFileOutput("scores.txt", Context.MODE_APPEND);
+            } catch (Exception e){
+                Log.e("files", "why gaiuhsdlouydsfbuf " + e.toString());
+            }
+        }
+        Log.d("files", toString() + "boom");
     }
 
     private boolean readScores(){
         //File scoreFile = new File(dir, "scores.txt");
+
         String line;
         String[] nameScore;
-        int i = 0;
+        BufferedReader buff;
+        /*File file = new File(appContext.getFileStreamPath());
+            if(!file.exists())
+                return false;*/
         try
         {
-            BufferedReader buff = new BufferedReader(new InputStreamReader(appContext.openFileInput("scores.txt")));
-            do {
-                line = buff.readLine();
-                nameScore = line.split(";");
-                scores.add(i, Integer.parseInt(nameScore[0]));
-                players.add(i, nameScore[1]);
-            } while (line != null);
+            buff = new BufferedReader(new InputStreamReader(appContext.openFileInput("scores.txt")));
         }
         catch(Exception e)
         {
-            Log.e("files", "Buggerit.");
+            Log.d("files", "No file to read");
             return false;
         }
-
+        try{
+            for(int i = 0; i < 10; i++)
+            {
+                line = buff.readLine();
+                if(line == null)
+                    return true;
+                nameScore = line.split(";");
+                scores.add(i, Integer.parseInt(nameScore[0]));
+                players.add(i, nameScore[1]);
+            }
+        } catch(Exception e){
+            Log.e("files", "reading " + e.toString());
+        }
         return true;
     }
 
@@ -71,7 +98,7 @@ public class ScoreBoard {
         if(position < 0)
             return false;
 
-        scores.set(position, new Integer(score));
+        scores.set(position, score);
         players.set(position, name);
         return true;
     }
